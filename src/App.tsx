a304afc947c1c9e2ade8159f69bb7bf5329ff91f
leon-do <leondo123@gmail.com>
2021-06-09 03:10:49 -0400

    log err

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import Web3 from "web3";
 function App() {
   useEffect(() => {
     initTorus();
-  }, []);
+  });
 
   const initTorus = async () => {
     const torus: Torus = new Torus({});
@@ -16,14 +16,15 @@ function App() {
       const signedMessage: string = await signMessage(web3);
       console.log(signedMessage);
     } catch (err) {
+      console.error(err);
       await torus.cleanUp();
-      window.location.reload();
+      initTorus();
     }
   };
 
   const signMessage = async (web3: Web3): Promise<string> => {
     const from: string = (await web3.eth.getAccounts())[0];
-    const expiration: number = Math.round(Date.now() / 1000 + 30)
+    const expiration: number = Math.round(Date.now() / 1000 + 30);
     const message: string = `${from}-${expiration}`;
     const signature: string = await web3.eth.personal.sign(message, from, "");
     return `${signature}-${message}`;
