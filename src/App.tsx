6c0abf00c092c2466a221bc6d3c868f44cd838e0
leon-do <leondo123@gmail.com>
2021-07-30 00:35:02 -0400

    damn this shit

--- src/App.tsx
+++ src/App.tsx
@@ -9,10 +9,12 @@ function App() {
   const openlogin = new OpenLogin({
     clientId: "BFFZOyDfs-K02CgDFpGVzX6A30EcaMCMoUoXkXwoyEbPuL7OZerAKu_2CY8EDxxefaaZaLf8rg1S7COegAyB1eM",
     network: "mainnet",
+    redirectUrl: `${window.origin}`,
   });
 
   useEffect(() => {
     initOpenLogin();
+    // eslint-disable-next-line
   }, []);
 
   const initOpenLogin = async () => {
