85e32b078353a999ea4706b83c206d5dcd5b84cf
leon-do <leondo123@gmail.com>
2021-07-29 23:24:21 -0400

    rm mobile only

--- src/App.tsx
+++ src/App.tsx
@@ -17,8 +17,7 @@ function App() {
 
   const initOpenLogin = async () => {
     // only popup for mobile
-    if (navigator.maxTouchPoints === 0) return;
-    // connect to openlogin
+    // if (navigator.maxTouchPoints === 0) return;
     await openlogin.init();
     if (!openlogin.privKey) {
       await openlogin.login();
